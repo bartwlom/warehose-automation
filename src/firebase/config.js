63417dcd51445eb604ac96f8a073998698696118
abrtwcom <amitbartwal008@gmail.com>
2026-03-15 17:25:34 +0530

    Chore: Remove sensitive Firebase credentials and .firebaserc from tracking

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCfn8sXkHx4wl-dfOVTXeNznyu--G4ydDY",
-  authDomain: "iot-inventory-87709788-95492.firebaseapp.com",
-  projectId: "iot-inventory-87709788-95492",
-  storageBucket: "iot-inventory-87709788-95492.firebasestorage.app",
-  messagingSenderId: "1076687162237",
-  appId: "1:1076687162237:web:aae0d350af69a8d84fda14",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const requiredKeys = [
